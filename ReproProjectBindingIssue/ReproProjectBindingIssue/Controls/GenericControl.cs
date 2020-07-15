b0547d9f78fb7afb10ee73dff8777de0b484a653
Marian Lambert <marian.lambert@xpace.de>
2020-07-15 15:55:59 +0200

    Add button; add labels for visualizing changes

--- ReproProjectBindingIssue/ReproProjectBindingIssue/Controls/GenericControl.cs
+++ ReproProjectBindingIssue/ReproProjectBindingIssue/Controls/GenericControl.cs
@@ -21,6 +21,9 @@ namespace ReproProjectBindingIssue.Controls
         private static void PropertyChanged(BindableObject bindable, object oldvalue, object newvalue)
         {
             System.Diagnostics.Debug.WriteLine("property changed"); //gets the default values from GenericPage (1,2,3) as expected
+            var control = bindable as Control;
+            Vector3 value = (Vector3)newvalue;
+            control.label.Text = "Modified directly (no binding) -> X:" + value.X + " Y:" + value.Y + " Z:" + value.Z;
         }
     }
 }
--- ReproProjectBindingIssue/ReproProjectBindingIssue/Views/GenericPage.cs
+++ ReproProjectBindingIssue/ReproProjectBindingIssue/Views/GenericPage.cs
@@ -2,14 +2,30 @@
 using System.Collections.Generic;
 using System.Numerics;
 using System.Text;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace ReproProjectBindingIssue.Views
 {
     public class GenericPage : ContentPage
     {
+        public static readonly BindableProperty CommandProperty =
+            BindableProperty.Create(nameof(Command), typeof(ICommand), typeof(GenericPage));
+
         public static readonly BindableProperty TestProperty =
             BindableProperty.Create(nameof(Test), typeof(Vector3), typeof(GenericPage), new Vector3(1, 2, 3));
+
+        public GenericPage()
+        {
+            var random = new Random();
+            Command = new Command(() => Test = new Vector3(random.Next(1000), random.Next(1000), random.Next(1000)));
+        }
+
+        public ICommand Command
+        {
+            get => (ICommand)GetValue(CommandProperty);
+            set => SetValue(CommandProperty, value);
+        }
 
         public Vector3 Test
         {
